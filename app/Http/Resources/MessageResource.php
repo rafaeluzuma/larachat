7208c4452a7a30da5237ce05164a9cacd419a8ec
rafael <rafaelrodrigues@bee.com.br>
2021-08-31 00:28:10 -0300

    API

---
+++ app/Http/Resources/MessageResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MessageResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'message' => $this->message,
+            'sender' => $this->sender,
+            'receiver' => new UserResource($this->receiver),
+            'date' => Carbon::make($this->created_at)->format('Y-m-d'),
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\ChatApiController;
+use App\Http\Controllers\Api\UserApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('v1')
+->middleware('auth:web')
+->group(function() {
+    Route::get('/users', [UserApiController::class, 'index']);
+    Route::get('/message/create', [ChatApiController::class, 'store']);
+});
+
+Route::get('/', function() {
+    return ['message' => 'ok'];
 });
